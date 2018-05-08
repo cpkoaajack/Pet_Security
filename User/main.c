b7a3024f8fc922616f25c46b945b11092e737923
pkyipab <pkyipab@ust.hk>
2018-05-09 03:42:05 +0800

    Algorithm

--- User/main.c
+++ User/main.c
@@ -11,7 +11,13 @@
 #define Buf2_Max 200 
 
 void GPIO_Config_LED(void);
-void displayMenu(char *,char *,char *,char *, char *);
+void displayMenu(void);
+void setSafeDist(void);
+void scanPassThrough(void);
+void alert(int alertSpeed);
+void importData(void);
+void exportData(void);
+
 void Delayus(int duration);
 void CLR_Buf2(void);
 void Second_AT_Command(char *b,char *a,u8 wait_time);
@@ -36,23 +42,28 @@ vu8 Timer0_start;
 vu8 Uart2_Start;
 vu8 Uart2_End;
 
-int32_t dist;
 u8  find_string_flag = 0;
 u8  has_call_flag = 0;
 
+int32_t dist = 0;	
+int32_t dist2 = 0;
+int32_t dist3 = 0;
+
+int32_t safeDist_S1 = -1;	
+int32_t safeDist_S2 = -1;	
+int32_t safeDist_S3 = -1;	
+
+int32_t warnTime = 0x0000;
+int32_t trigTime = 0x0000;
+
+int32_t pass1 = 0;
+int32_t pass2 = 0;
+int32_t pass3 = 0;
+
+int32_t boolImport = 0; //indicate whether import function is called
+
 int main(void)
 {	
-
-	int ret ;
-	int32_t dist;	
-	int32_t dist2;
-	int32_t saveDist;	
-	int sDist = 0x0000;
-	int warnTime = 0x0000;
-	int trigTime = 0x0000;
-	int dev = 0;
-	
-	int safe = 1;
 	
 	LCD_INIT();	
 	InitHCSR04();
@@ -60,55 +71,17 @@ int main(void)
 	USART2_Init_Config(115200);
 	Timer2_Init_Config();
 	
-	while(1) {
-		char saveDist[4];
-		char warned[4];
-		char triggered[4];
-		char distance[4];
-		char distance2[4];	  
+	while(1) {  
 
 		dist = HCSR04GetDistanceS1();
 		dist2 = HCSR04GetDistanceS2();
 		
-		if(dist < 0) {
-			dist = 0;
-		} 
-		
-		if(dist2 < 0) {
-			dist2 = 0;
-		}
-		
-		
-		if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0) == 1){
-			//UART2_SendString(phone);
-			sDist = dist - 20;
-			if(dist < 0) {
-				sDist = 0;
-			}
-			sprintf(saveDist, "%03i", sDist );
-		}
-		
-		if(dist < sDist) {
-				LCD_DrawString(200, 200, "Fail");
-				dev = dev + 1;
-			
-				warnTime = dev % 10;
-				macBEEP( ON );			  
-				Delayus(100000);	
-				macBEEP( OFF );		  
-				Delayus(100000);
-				safe = 0;
-		} else if (dist2 < sDist && safe == 0){
-			
-				Send_Text_Sms((char*)content);
-		}
-		
-		sprintf(distance, "%03i", dist);
-		sprintf(distance2, "%03i", dist2);
-		sprintf(warned, "%03i", warnTime);
-		sprintf(triggered, "%03i", trigTime);
-		
-		displayMenu(distance,distance2,warned,triggered,saveDist);
+		importData();
+		setSafeDist();
+		scanPassThrough();
+	
+		exportData();
+		displayMenu();
 		
 	}
 
@@ -137,30 +110,175 @@ void GPIO_Config_LED(void) {
   GPIO_SetBits(GPIOB, GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_5);
 }
 
-void displayMenu(char * dist, char * dist2, char * warn, char * trig, char * safeDist) {
+
+
+
+
+void displayMenu() {	
+	char distance[4];
+	char distance2[4];	  
+	char warn_num[4];
+	char trigger_num[4];
+	char safeDistChar[4];
+	
+	sprintf(distance, "%03i", dist);
+	sprintf(distance2, "%03i", dist2);
+	sprintf(warn_num, "%03i", warnTime);
+	sprintf(trigger_num, "%03i", trigTime);
+	sprintf(safeDistChar, "%03i", safeDist_S1);
+	
 	LCD_DrawString(15, 5, "Pet Security Monitor");
 	LCD_DrawString(15, 25, "Yip Pak Kin & Ko Chung Pong");
 	LCD_DrawString(15, 45, "Distance of Sensor 1:");
-	LCD_DrawString(190, 45, dist);
+	LCD_DrawString(190, 45, distance);
 	LCD_DrawString(215, 45, "mm    ");
 	LCD_DrawString(15, 65, "Distance of Sensor 2:");
-	LCD_DrawString(190, 65, dist2);
+	LCD_DrawString(190, 65, distance2);
 	LCD_DrawString(215, 65, "mm    ");
 	LCD_DrawString(15, 95, "Warning Times: ");
-	LCD_DrawString(190, 95, warn);
+	LCD_DrawString(190, 95, warn_num);
 	LCD_DrawString(15, 115, "Danger TImes: ");
-	LCD_DrawString(190, 115, trig);
+	LCD_DrawString(190, 115, trigger_num);
 	
 	LCD_DrawString(0, 130, "******************************");
 	LCD_DrawString(100, 145, "Menu");
 	LCD_DrawString(15, 160, "1) Call Out Test");
 	LCD_DrawString(15, 175, "2) Export data");
 	LCD_DrawString(15, 230, "Saved Safe Distance :");
-	LCD_DrawString(190, 230, safeDist);
+	LCD_DrawString(190, 230, safeDistChar);
 	LCD_DrawString(215, 230, "mm    ");
 	
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+void scanPassThrough(){
+		
+		if(dist < 0) {
+			dist = 0;
+		} 
+		
+		if(dist2 < 0) {
+			dist2 = 0;
+		}
+		
+		if(dist3 < 0) {
+			dist3 = 0;
+		}
+		
+		
+		if(dist < safeDist_S1) {
+				pass1++;
+		} else if (dist2 < safeDist_S2){
+				pass2++;
+		} else if (dist3 < safeDist_S3) {
+				pass3++;
+		}
+		
+		
+		if(pass1 > 0 && pass2 == 0 && pass3 == 0) {
+			alert(100000);
+		}	else if(pass1 != 0 && pass1 % 2 == 0 && pass2 == 0) {
+			pass1 = 0;
+		} else if(pass1 > 0 && pass2 > 0 && pass3 == 0 ){
+			alert(10000);
+		} else if(pass2 != 0 && pass2 % 2 == 0 && pass1 != 0) {
+			alert(100000);
+			pass2 = 0;
+		} else if(pass1 > 0 && pass2 > 0 && pass3 > 0) {
+			//call
+			alert(5000);
+		} else if(pass3 != 0 && pass3 % 2 == 0 && pass2 != 0) {
+			pass3 = 0;
+		}
+}
+
+
+
+void setSafeDist() {
+	
+	if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0) == 1){
+			safeDist_S1 = dist - 20;
+			safeDist_S2 = dist - 20;
+			safeDist_S3 = dist - 20;
+		
+			if(safeDist_S1 < 0) {
+				safeDist_S1 = 0;
+			}
+			if(safeDist_S2 < 0) {
+				safeDist_S2 = 0;
+			}
+			if(safeDist_S3 < 0) {
+				safeDist_S3 = 0;
+			}
+		}
+}
+
+
+
+
+
+
+void alert(int alertSpeed){
+		macBEEP( ON );			  
+		Delayus(alertSpeed);  //100000 by jack	
+		macBEEP( OFF );		  
+		Delayus(alertSpeed);
+}
+
+/***********************************************************
+ *
+ *	Import SD card Data
+ *
+ * 	Press K2 before setting SafeDistance (K1) -> Import Data
+ *	
+ *  Press K2 After setting SafeDistance (K1) -> Export Data
+ *
+ ************************************************************
+*/
+
+void importData(){
+	
+	if(GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_13) == 1 && boolImport == 0){
+		/*
+		*		import data
+		*
+		*/
+	
+		GPIO_ResetBits(GPIOB, GPIO_Pin_1);
+		boolImport = 1;
+	}
+}
+
+void exportData() {
+	
+	if(GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_13) == 1 && boolImport == 1){
+		/*
+		*		export data
+		*
+		*/
+		GPIO_ResetBits(GPIOB, GPIO_Pin_5);
+	}
+}
+
+
+/*****************************
+ *
+ *
+ *	NDS CODE BELOW
+ *
+ *****************************
+*/
 void USART2_IRQHandler(void)                	
 {
 			u8 Res=0;
@@ -285,9 +403,9 @@ void Set_Text_Mode(void)
 
 void Send_Text_Sms(char* msg)
 {
-	Set_Text_Mode();
-	
 	char temp[50]="AT+CMGS=";
+	
+	Set_Text_Mode();
 	
 	strcat(temp,(char*)phone_sms);
 	Second_AT_Command(temp,">",3);
